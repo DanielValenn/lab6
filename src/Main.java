c6620555f8af8ea9da08236ec7936cbd4b3b397a
Nedelea Daniel Valentin <daniel.valentin.ndl@gmail.com>
2025-03-31 10:21:06 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Calculator c = new Calculator();
-        c.setX(12);
+        Calculator c = new Calculator(12);
         c.ADUNARE(3);
         c.SCADERE(2);
         c.getREZULTAT();
