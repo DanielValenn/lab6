f204c6766b2c6ff68bb5636ea0c3f7a35552e5db
Nedelea Daniel Valentin <daniel.valentin.ndl@gmail.com>
2025-03-31 10:20:09 +0300

    k

---
+++ src/Calculator.java
@@ -0,0 +1,38 @@
+public class Calculator {
+    double x;
+
+    public Calculator(double x) {
+        this.x = x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void ADUNARE(double a) {
+        x += a;
+    }
+
+    public void SCADERE(double a) {
+        x -= a;
+    }
+
+    public void INMULTIRE(double a) {
+        x *= a;
+    }
+
+    public void IMPARTIRE(double a) {
+        if (a == 0){
+            throw new RuntimeException("Nu poti sa imparti cu zero!");
+        }
+        x /= a;
+    }
+
+    public void getREZULTAT() {
+        System.out.println(x);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Calculator c = new Calculator();
+        c.setX(12);
+        c.ADUNARE(3);
+        c.SCADERE(2);
+        c.getREZULTAT();
+
+    }
+}
